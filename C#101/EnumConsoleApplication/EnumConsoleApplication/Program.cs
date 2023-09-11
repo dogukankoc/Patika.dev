1b725a2325cef5b89a767f417de815803a0a44fd
Dogukan Koc <dk.dogukankoc@gmail.com>
2023-09-11 23:01:19 +0300

    EnumConsoleApplicationCreated

---
+++ C#101/EnumConsoleApplication/EnumConsoleApplication/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine(Gunler.Pazartesi); // Pazartesi
+Console.WriteLine((int)Gunler.Pazartesi); // 0
+enum Gunler
+{
+    Pazartesi,
+    Sali,
+    Carsamba,
+    Persembe,
+    Cuma,
+    Cumartesi,
+    Pazar
+};
+
